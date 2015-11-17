7ba9c5c3fd282f6c876976e050116bebaa6b9f59
jonathan <durandjonathan2@gmail.com>
2015-11-17 17:52:48 +0100

    OGR2OGR VRT OK

--- actual/tree/ig/vecteur/parcs-naturels-regionaux.point.sh
+++ actual/tree/ig/vecteur/parcs-naturels-regionaux.point.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ogr2ogr -f "ESRI Shapefile" shp/ parcs-naturels-regionaux.lonlat.vrt
-
+ogr2ogr -f "ESRI Shapefile" balades/ parcs-naturels-regionaux.balades.vrt
+ogr2ogr -f "ESRI Shapefile" balades_vlo/ parcs-naturels-regionaux.balades-velo.vrt 
+ogr2ogr -f "ESRI Shapefile" balades_vlo2/ parcs-naturels-regionaux.balades-velo2.vrt
+ogr2ogr -f "ESRI Shapefile" shp_lambert93/ parcs-naturels-regionaux.lambert93.vrt
